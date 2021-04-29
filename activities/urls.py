82b2062d5c479d81b59bdb2f8c4b152ef92a27c5
Susan Dangol <49741990+susan5591@users.noreply.github.com>
2021-04-29 13:47:32 +0545

    crud operation

--- activities/urls.py
+++ activities/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("",views.home ),
     path("home/",views.home , name="home"),
     path("create/",views.create , name="create"),
+    path("detail/<str:pk>/", views.detail,name="detail"),
+    path("edit/<str:pk>", views.edit, name="edit"),
+    path("delete/<str:pk>", views.delete, name="delete"),
 ]
